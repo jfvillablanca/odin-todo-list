043073abf4311ac1fb7716f6dc45fccb8128c161
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-17 13:00:05 +0800

    Set to dev mode and add inline source map devtool

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   output: {
-    filename: 'main.js',
+    filename: '[name].main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
