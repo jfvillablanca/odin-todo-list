7fd231fe531947a6f2b6a2ecd2c91d059ed8e7db
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-19 19:13:47 +0800

    Add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Todo List',
+    }),
+  ],
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
   output: {
-    filename: '[name].main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
