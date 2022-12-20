7de360f5bd599654c51a3c6e8688d6dcfaff1b5b
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-20 13:38:46 +0800

    Rewrite todoNote prototype

--- src/index.js
+++ src/index.js
@@ -4,10 +4,49 @@
 // 3. fn: modify todo note
 // 4* fn: store note in folder
 
-const TodoNote = (function(){
-  const proto = {
-    title: "",
-    dueDate:
+const todoNote = (title, dueDate, priority, description, isStarred) => {
+  let _title       = title || "What are you trying to accomplish today?";
+  let _dueDate     = dueDate || "";
+  let _priority    = priority || "low";
+  let _description = description || "Feel free to describe :)";
+  let _isStarred   = isStarred || false; 
+
+  return {
+    get title() {
+      return _title;
+    },
+    set setTitle(value) {
+    // logic / error handling
+      _title = value;
+    },
+    get dueDate() {
+      return _dueDate;
+    },
+    set setDueDate(value) {
+    // logic / error handling
+      _dueDate = value;
+    },
+    get priority() {
+      return _priority;
+    },
+    set setPriority(value) {
+    // logic / error handling
+      _priority = value;
+    },
+    get description() {
+      return _description;
+    },
+    set setDescription(value) {
+    // logic / error handling
+      _description = value;
+    },
+    get isStarred() {
+      return _isStarred;
+    },
+    set setStarred(value) {
+    // logic / error handling
+      _isStarred = value;
+    },
   }
   return Object.assign(Object.create(proto));
 }
