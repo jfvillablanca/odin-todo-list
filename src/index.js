0c5b5be4d036f1d2390118d079f2c0f3d50fcb3b
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-20 10:44:10 +0800

    Set TODO

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-const TodoNote = function() {
+// TODO: 
+// 1. fn: new todo note
+// 2. fn: delete todo note
+// 3. fn: modify todo note
+// 4* fn: store note in folder
+
+const TodoNote = (function(){
   const proto = {
     title: "",
     dueDate:
