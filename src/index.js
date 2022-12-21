855c9dc82bbe6b3a8d4f1d6d2384a018c4f7729a
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-21 10:00:52 +0800

    Refactor todoNote factory function

--- src/index.js
+++ src/index.js
@@ -4,56 +4,26 @@
 // 3. fn: modify todo note
 // 4* fn: store note in folder
 
-const todoNote = (title, dueDate, priority, description, isStarred) => {
-  let _title       = title || "What are you trying to accomplish today?";
-  let _dueDate     = dueDate || "";
-  let _priority    = priority || "low";
-  let _description = description || "Feel free to describe :)";
-  let _isStarred   = isStarred || false; 
+const objFactory = () => {
+  const attrs = {};
 
   return {
-    get title() {
-      return _title;
+    get (name) {
+      return attrs[name];
     },
-    set setTitle(value) {
-    // logic / error handling
-      _title = value;
-    },
-    get dueDate() {
-      return _dueDate;
-    },
-    set setDueDate(value) {
-    // logic / error handling
-      _dueDate = value;
-    },
-    get priority() {
-      return _priority;
-    },
-    set setPriority(value) {
-    // logic / error handling
-      _priority = value;
-    },
-    get description() {
-      return _description;
-    },
-    set setDescription(value) {
-    // logic / error handling
-      _description = value;
-    },
-    get isStarred() {
-      return _isStarred;
-    },
-    set setStarred(value) {
-    // logic / error handling
-      _isStarred = value;
+    set (name, value) {
+      attrs[name] = value;
     },
   }
 }
 
-const folderTodo = () => {
-  let _name      = "Folder Name";
-  let _isDefault = false;
-  const _notes   = [];
+const todoNoteFactory = Object.assign({}, objFactory(), {
+  title: "What are you trying to accomplish today?",
+  dueDate: new Date(Date.now()),
+  priority: "low",
+  description: "Feel free to describe :)",
+  isStarred: false,
+});
 
   return {
     get name() {
