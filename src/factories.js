61a034e076c7228373ab8adcb80a2f61216e25e7
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-23 17:04:38 +0800

    Refactor 'createTodoNote' to adhere to functional programming paradigm

--- src/factories.js
+++ src/factories.js
@@ -79,8 +79,56 @@ export function createFolder({name = "Folder Name", isStarred = false} = {}) {
   }
 }
 
-export function createTodoNote() {
-  return Object.assign({}, todoNoteDefaultProps, todoNoteMethods,) 
+export function createTodoNote({
+  name = "What are you trying to accomplish today?",
+  dueDate = new Date(Date.now()),
+  priority = "low",
+  description = "Feel free to describe :)",
+  isStarred = false,
+  folder = defaultFolder,
+  } = {}) {
+  
+  const fields = {
+    name: name,
+    dueDate: dueDate,
+    priority: priority,
+    description: description,
+    isStarred: isStarred,
+    folder: folder,
+  };
+
+  const getName = () => {
+    return fields.name;
+  }
+
+  const setName = (value) => {
+    fields.name = value;
+  };
+
+  const getStarStatus = () => {
+    return fields.isStarred;
+  }
+
+  const toggleStar = () => {
+    (fields.isStarred == false) ? fields.isStarred = true : fields.isStarred = false;
+  }
+
+  const getFolder = () => {
+    return fields.folder;
+  }
+
+  const setFolder = (value) => {
+    fields.folder = value;
+  };
+
+  return {
+    getName,
+    setName,
+    getStarStatus,
+    toggleStar,
+    getFolder,
+    setFolder,
+  }
 }
 
 export const dir = DirectoryUtils();
