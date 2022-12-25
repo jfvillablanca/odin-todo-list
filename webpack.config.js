20414dcf257c1070859a6d9fa66bc9c44185aa74
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-25 09:48:53 +0800

    Chore: code formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/js/index.js',
+  mode: "development",
+  entry: "./src/js/index.js",
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Todo List',
+      template: "./src/index.html",
     }),
   ],
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
-    static: './dist',
+    static: "./dist",
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
 };
