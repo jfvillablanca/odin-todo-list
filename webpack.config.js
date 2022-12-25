8d39e8367693f2a511d4ae0539b1a784b06f3812
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-25 09:55:15 +0800

    Webpack: Add image loader

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,11 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
