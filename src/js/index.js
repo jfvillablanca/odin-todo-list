fdc30a4929e0aa1fa44a2a6c3e80a5fed9b860f0
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-25 10:01:09 +0800

    Import boilerplate css to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,11 @@
 // TODO:
+// Application-level Features:
+// - 
+// Low-level (immediate) features:
+// - 
+import './../css/skeleton.css';
+import './../css/normalize.css';
+
 import { dir, defaultFolder, } from './factories.js';
 
 // Controller Logic
