e078dd82d133cfac8f4cd716a1776d4462056ccf
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-27 14:41:45 +0800

    Create elements on basic layout

--- src/js/dom-generator.js
+++ src/js/dom-generator.js
@@ -2,3 +2,10 @@ import './../css/skeleton.css';
 import './../css/normalize.css';
 import './../css/styles.css';
 
+const container = document.querySelector(".container");
+const projectSidebar = document.createElement("div");
+projectSidebar.classList.add("project-sidebar");
+const main = document.createElement("div");
+main.classList.add("main");
+container.appendChild(projectSidebar);
+container.appendChild(main);
