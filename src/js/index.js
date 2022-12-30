2e110f0022592936fdb11064595fa78f74ad3a64
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-30 09:29:10 +0800

    Add filler data for tests

--- src/js/index.js
+++ src/js/index.js
@@ -11,3 +11,26 @@ import { loadDOM, sidebarCollapse } from './dom-generator.js';
 // These are just tests
 loadDOM();
 sidebarCollapse();
+
+// Filler data:
+defaultFolder.addNewTodo();
+defaultFolder.addNewTodo();
+defaultFolder.addNewTodo();
+defaultFolder.addNewTodo();
+defaultFolder.addNewTodo();
+
+const folder1 = dir.addFolder({ name: "Work" });
+const folder2 = dir.addFolder({ name: "Personal" });
+dir.addFolder();
+
+folder1.addNewTodo();
+folder1.addNewTodo();
+folder1.addNewTodo();
+
+folder2.addNewTodo();
+
+console.log("def Folder length");
+console.log(defaultFolder.get("notes").length);
+console.log("dir length");
+console.log(dir.getFolderList().length);
+// end of filler data
