f779297593fa69f8735d13c88d05530cccb57807
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-30 13:00:30 +0800

    Insert note-creation to storageLogic() IIFE

--- src/js/index.js
+++ src/js/index.js
@@ -17,33 +17,16 @@ const PubSub = require("vanilla-pubsub");
 loadDOM();
 sidebarCollapse();
 
-const addNewNote = (folder) => {
-  const newNote = folder.addNewTodo();
-  PubSub.publish("new-note-to-folder", {
-    folder: folder,
-    note: newNote,
-  });
-};
-// Filler data:
-
-addNewNote(defaultFolder);
-addNewNote(defaultFolder);
-addNewNote(defaultFolder);
-addNewNote(defaultFolder);
-addNewNote(defaultFolder);
-
-const folder1 = dir.addFolder({ name: "Work" });
-const folder2 = dir.addFolder({ name: "Personal" });
-dir.addFolder();
-
-folder1.addNewTodo();
-folder1.addNewTodo();
-folder1.addNewTodo();
-
-folder2.addNewTodo();
-
-console.log("def Folder length");
-console.log(defaultFolder.get("notes").length);
-console.log("dir length");
-console.log(dir.getFolderList().length);
-// end of filler data
+(function storageLogic() {
+  const addNewNote = (project) => {
+    const newNote = project.addNewTodo();
+    const projectID = project.get("id");
+    const projectNoteCount = project.get("notes").length;
+    PubSub.publish("new-note-to-project", {
+      project: project,
+      projectID,
+      projectNoteCount,
+      newNote,
+    });
+  };
+})();
