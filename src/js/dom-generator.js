b4d49e7acb7b6dd064b25aa8b9f5429a88bc817d
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-01-03 08:29:03 +0800

    Rewrite starListeners()

--- src/js/dom-generator.js
+++ src/js/dom-generator.js
@@ -315,25 +315,26 @@ export const changeProjectStarOpacity = () => {
 
 export const starListeners = () => {
   const stars = document.querySelectorAll(".listen-to-stars");
+  const clickEvent = (event) => {
+    // NOTE: For note-startoggle__star
+    if (event.target.tagName === "DIV") {
+      starToggle(event.target.firstChild);
+    }
+    // NOTE: For project-item-star
+    if (
+      [...event.target.classList].includes("project-item-star") &&
+      event.target.getAttribute("src") === StarOn
+    ) {
+      event.target.classList.remove("starred");
+    } else if (
+      [...event.target.classList].includes("project-item-star") &&
+      event.target.getAttribute("src") === StarOff
+    ) {
+      event.target.classList.add("starred");
+    }
+    starToggle(event.target);
+  };
   stars.forEach((star) => {
-    star.addEventListener("click", (event) => {
-      // NOTE: For note-startoggle__star
-      if (event.target.tagName === "DIV") {
-        starToggle(event.target.firstChild);
-      }
-      // NOTE: For project-item-star
-      if (
-        [...event.target.classList].includes("project-item-star") &&
-        event.target.getAttribute("src") === StarOn
-      ) {
-        event.target.classList.remove("starred");
-      } else if (
-        [...event.target.classList].includes("project-item-star") &&
-        event.target.getAttribute("src") === StarOff
-      ) {
-        event.target.classList.add("starred");
-      }
-      starToggle(event.target);
-    });
+    star.addEventListener("click", clickEvent);
   });
 };
